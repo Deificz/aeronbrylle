df36640b1de7a304eb0e1a70e2704d860374ff12
Deificz <99456747+Deificz@users.noreply.github.com>
2023-02-22 12:18:52 +0800

    Sliders and Sub-titles Tweaks

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-
-var swiper = new Swiper('.work-content', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: 3,
-    loop: true,
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 50,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
+var swiper = new Swiper('.social-content', {
+    
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
   });
 
-  var swiper = new Swiper('.social-content', {
-    
+  var swiper = new Swiper('.work-content', {
     slidesPerView: 3,
-    grabCursor: true,
-    spaceBetween: 10,
-    slidesPerGroup: 3,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: '.swiper-pagination',
-
-    },
+      spaceBetween: 30,
+      freeMode: true,
+      loop: true,
+      grabCursor: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
   });
