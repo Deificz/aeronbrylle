8a8cbf08f6cbaebf4aaa6d03514bcf98857e16f8
Deificz <99456747+Deificz@users.noreply.github.com>
2023-07-06 11:16:12 +0800

    Updated email and moved script location

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+//FORM SCRIPT
+const scriptURL = 'https://script.google.com/macros/s/AKfycbz_7NafZ7rVlmwgKxWGe3x1SdzcGTgHst0tyoel8y2RncmDLVl-TseLjwkEovRpdwt8TA/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const message = document.getElementById("doneMsg")
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            message.innerHTML = "Message sent Successfully"
+            setTimeout(function(){
+                message.innerHTML = ""
+            },3000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
+
+//MENU SCRIPT SECTION
+var sideMenu = document.getElementById("sideMenu");
+
+    function openMenu(){
+        sideMenu.style.right = "0";
+    }
+    function closeMenu(){
+        sideMenu.style.right = "-200px";
+    }
+
+//PROJECT UI SCRIPT
 var swiper = new Swiper('.social-content', {
     
   effect: 'coverflow',
