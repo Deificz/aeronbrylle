19a7ef4d4283e19198f7bbfbb345b0f831a5eaea
Deificz <99456747+Deificz@users.noreply.github.com>
2023-07-12 18:10:34 +0800

    Fixed the placement of some codes

--- script.js
+++ script.js
@@ -24,6 +24,22 @@ var sideMenu = document.getElementById("sideMenu");
     function closeMenu(){
         sideMenu.style.right = "-200px";
     }
+    
+//ABOUT TABS
+var tablinks= document.getElementsByClassName("tab-links");
+var tabcontents= document.getElementsByClassName("tab-contents");
+
+    function opentab(tabname)
+    {
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+    }
 
 //ON-SCROLL ANIMATION 
 AOS.init();
