2cb8eaef4173349160edcf696ccda2ae259241e9
Deificz <99456747+Deificz@users.noreply.github.com>
2023-08-06 22:38:26 +0800

    Fixed some styling

--- script.js
+++ script.js
@@ -21,34 +21,28 @@ const loadAnim = (() => {
 })();
 
 //MENU SECTION ANIMATION
-const menuShow = (() => {
-  var sideMenu = document.getElementById("sideMenu");
+var sideMenu = document.getElementById("sideMenu");
 
-  function openMenu(){
-      sideMenu.style.right = "0";
-  }
-  function closeMenu(){
-      sideMenu.style.right = "-200px";
-  }
-})();
-
+function openMenu(){
+    sideMenu.style.right = "0";
+}
+function closeMenu(){
+    sideMenu.style.right = "-200px";
+}
 //ABOUT TABS
-const aboutTabsSwitch = (() => {
-  var tablinks= document.getElementsByClassName("tab-links");
-  var tabcontents= document.getElementsByClassName("tab-contents");
-  
-      function opentab(tabname)
-      {
-          for(tablink of tablinks){
-              tablink.classList.remove("active-link");
-          }
-          for(tabcontent of tabcontents){
-              tabcontent.classList.remove("active-tab");
-          }
-          event.currentTarget.classList.add("active-link");
-          document.getElementById(tabname).classList.add("active-tab");
-      }
-})();
+var tablinks= document.getElementsByClassName("tab-links");
+var tabcontents= document.getElementsByClassName("tab-contents");
+
+    function opentab(tabname){
+        for(tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+    }
 
 //PROJECT ANIMATION 
 const slideAnim = (() => {
