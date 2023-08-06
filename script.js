e2a9ad05a82b6a4adaf14ebf17f8c0402a684368
Deificz <99456747+Deificz@users.noreply.github.com>
2023-08-06 21:40:25 +0800

    Added navbar and refactored script structure

--- script.js
+++ script.js
@@ -1,103 +1,126 @@
-//LOAD ANIMATION
-$(window).on("load",function(){
-  $(".loader-wrapper").fadeOut("slow");
-  $("body").removeClass("preload");
+//STICKY NAVBAR
+window.addEventListener('scroll', function() {
+  const ul = document.getElementById(`sideMenu`);
+  const offset = window.scrollY;
+ 
+  // Change the class based on the scroll position
+  if (offset > 50) {
+    ul.classList.add('sticky');
+
+  } else {
+    ul.classList.remove('sticky');
+  }
 });
 
+//LOAD ANIMATION
+const loadAnim = (() => {
+  $(window).on("load",function(){
+    $(".loader-wrapper").fadeOut("slow");
+    $("body").removeClass("preload");
+  });
+})();
+
 //MENU SECTION ANIMATION
-var sideMenu = document.getElementById("sideMenu");
+const menuShow = (() => {
+  var sideMenu = document.getElementById("sideMenu");
 
-    function openMenu(){
-        sideMenu.style.right = "0";
-    }
-    function closeMenu(){
-        sideMenu.style.right = "-200px";
-    }
-    
+  function openMenu(){
+      sideMenu.style.right = "0";
+  }
+  function closeMenu(){
+      sideMenu.style.right = "-200px";
+  }
+})();
+
 //ABOUT TABS
-var tablinks= document.getElementsByClassName("tab-links");
-var tabcontents= document.getElementsByClassName("tab-contents");
-
-    function opentab(tabname)
-    {
-        for(tablink of tablinks){
-            tablink.classList.remove("active-link");
-        }
-        for(tabcontent of tabcontents){
-            tabcontent.classList.remove("active-tab");
-        }
-        event.currentTarget.classList.add("active-link");
-        document.getElementById(tabname).classList.add("active-tab");
-    }
+const aboutTabsSwitch = (() => {
+  var tablinks= document.getElementsByClassName("tab-links");
+  var tabcontents= document.getElementsByClassName("tab-contents");
+  
+      function opentab(tabname)
+      {
+          for(tablink of tablinks){
+              tablink.classList.remove("active-link");
+          }
+          for(tabcontent of tabcontents){
+              tabcontent.classList.remove("active-tab");
+          }
+          event.currentTarget.classList.add("active-link");
+          document.getElementById(tabname).classList.add("active-tab");
+      }
+})();
 
 //PROJECT ANIMATION 
-var swiper = new Swiper('.social-content', {
+const slideAnim = (() => {
+  var swiper = new Swiper('.social-content', {
     
-  effect: 'coverflow',
-  grabCursor: true,
-  slidesPerView: 4,
-  centeredSlide: true,
-  centeredSlidesBounds: true,
-  loopFillGroupWithBlank: true,
-  navigation: { 
-    nextEl: '.swiper-button-next', 
-    prevEl: '.swiper-button-prev' 
-  },
-  
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: true,
-  },
-  320: {
-    slidesPerView: 2,
-    spaceBetween: 1
-  },
-  1280: {
+    effect: 'coverflow',
+    grabCursor: true,
     slidesPerView: 4,
-    spaceBetween: 10
-  },
-  });
-
-  var swiper = new Swiper('.work-content', {
-      slidesPerView: 3,
-      spaceBetween: 30,
-      freeMode: true,
-      grabCursor: true,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      breakpoints: {
-        320: {
-          slidesPerView: 2,
-          spaceBetween: 10
+    centeredSlide: true,
+    centeredSlidesBounds: true,
+    loopFillGroupWithBlank: true,
+    navigation: { 
+      nextEl: '.swiper-button-next', 
+      prevEl: '.swiper-button-prev' 
+    },
+    
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 1
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 10
+    },
+    });
+  
+    var swiper = new Swiper('.work-content', {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        freeMode: true,
+        grabCursor: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
         },
-        1280: {
-          slidesPerView: 3,
-          spaceBetween: 10
-        },}
-  });
+        breakpoints: {
+          320: {
+            slidesPerView: 2,
+            spaceBetween: 10
+          },
+          1280: {
+            slidesPerView: 3,
+            spaceBetween: 10
+          },}
+    });
+})();
 
 //FORM SCRIPT
-const scriptURL = 'https://script.google.com/macros/s/AKfycbz_7NafZ7rVlmwgKxWGe3x1SdzcGTgHst0tyoel8y2RncmDLVl-TseLjwkEovRpdwt8TA/exec'
-const form = document.forms['submit-to-google-sheet']
-const message = document.getElementById("doneMsg")
-form.addEventListener('submit', e => {
-  e.preventDefault()
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => {
-        message.innerHTML = "Message sent Successfully"
-        setTimeout(function(){
-            message.innerHTML = ""
-        },3000)
-        form.reset()
-    })
-    .catch(error => console.error('Error!', error.message))
-})
-
+const formScript = (() => {
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbz_7NafZ7rVlmwgKxWGe3x1SdzcGTgHst0tyoel8y2RncmDLVl-TseLjwkEovRpdwt8TA/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const message = document.getElementById("doneMsg")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+          message.innerHTML = "Message sent Successfully"
+          setTimeout(function(){
+              message.innerHTML = ""
+          },3000)
+          form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
+})();
 
 //TEMPORARY
 const btnResume = document.querySelector(`.btnResume`);
