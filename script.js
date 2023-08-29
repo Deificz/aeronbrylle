17be637f9bd69cfd55401345ad2eb01db1b1bb05
Deificz <99456747+Deificz@users.noreply.github.com>
2023-08-29 14:49:41 +0800

    Removed social section

--- script.js
+++ script.js
@@ -62,36 +62,6 @@ const slideAnim = (() => {
     })
     
   })
-
-  var swiper = new Swiper('.social-content', {
-    
-    effect: 'coverflow',
-    grabCursor: true,
-    slidesPerView: 4,
-    centeredSlide: true,
-    centeredSlidesBounds: true,
-    loopFillGroupWithBlank: true,
-    navigation: { 
-      nextEl: '.swiper-button-next', 
-      prevEl: '.swiper-button-prev' 
-    },
-    
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 1
-    },
-    1280: {
-      slidesPerView: 4,
-      spaceBetween: 10
-    },
-    });
   
     var swiper = new Swiper('.work-content', {
         slidesPerView: 3,
